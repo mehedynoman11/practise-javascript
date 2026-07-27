60d11ecc6d2deb7f17ba7432480e09b842ac781d
mehedynoman11 <mehedynoman11@gmail.com>
2026-07-27 10:17:21 +0600

    get sum averaga and filtered by condition

--- numberchk.js
+++ numberchk.js
@@ -10,4 +10,33 @@ for (let i = 1; i < numbers.length; i++) {
         largest = numbers[i];
     }
 }
-console.log("Largest value is ->> ", largest);
+// console.log("Largest value is ->> ", largest);
+
+//! Sum average 
+
+const number_2 = [10, 25, 30, 42, 74, 92];
+
+let sum = 0;
+for (let i = 0; i < number_2.length; i++) {
+    // console.log(number_2[i])
+    sum += number_2[i];
+}
+// console.log(sum)
+let sumAverage = sum / number_2.length;
+// console.log(sumAverage)
+
+
+function getSumAverage(arr, value) {
+    let filteredArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > value) {
+            // console.log(arr[i])
+            filteredArray.push(arr[i]);
+        }
+    }
+    return filteredArray;
+
+}
+
+let result = getSumAverage([10, 25, 30, 42, 74, 92], 40);
+console.log(result);
